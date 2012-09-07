1e12c3adf6b0279748186fa9ca23b9d8c3daa853
mortis1337 <bekkelund@gmail.com>
2012-09-07 13:03:43 +0200

    Added GPL info

--- merlin.php
+++ merlin.php
@@ -1,4 +1,22 @@
 <? 
+
+# 	 The nagios-dashboard was written by Morten Bekkelund & Jonas G. Drange in 2010 
+#
+#    Copyright (C) 2010 Morten Bekkelund & Jonas G. Drange
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    See: http://www.gnu.org/copyleft/gpl.html
+
+
 $con = mysql_connect("localhost", "user", "password") or die("<h3><font color=red>Could not connect to the database!</font></h3>");
 $db = mysql_select_db("merlin", $con);
 
--- nagios.php
+++ nagios.php
@@ -1,4 +1,22 @@
 <?php 
+
+#        The nagios-dashboard was written by Morten Bekkelund & Jonas G. Drange in 2010
+#
+#    Copyright (C) 2010 Morten Bekkelund & Jonas G. Drange
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    See: http://www.gnu.org/copyleft/gpl.html
+
+
     $refreshvalue = 10; //value in seconds to refresh page
     $pagetitle = "Operations Nagios Dashboard";
 ?>
